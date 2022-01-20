16b52eeb913c58bc79f3ef72ff66845902fefd17
Pavel Sidorovich <pavsid2001@mail.ru>
2022-01-20 16:14:25 +0300

    cleanup code

--- persistence/src/main/resources/db/test-data.sql
+++ persistence/src/main/resources/db/test-data.sql
@@ -1,3 +1,6 @@
-INSERT INTO public.tag (name) VALUES ('Gift');
-INSERT INTO public.tag (name) VALUES ('Car');
-INSERT INTO public.tag (name) VALUES ('Third tag');
+INSERT INTO public.tag (name)
+VALUES ('Gift');
+INSERT INTO public.tag (name)
+VALUES ('Car');
+INSERT INTO public.tag (name)
+VALUES ('Third tag');
--- service/src/main/java/com/epam/esm/gcs/service/CRDService.java
+++ service/src/main/java/com/epam/esm/gcs/service/CRDService.java
@@ -1,7 +1,6 @@
 package com.epam.esm.gcs.service;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CRDService<T> {
 
