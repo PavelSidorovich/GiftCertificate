d95d52a5c01e40aadac49a8f7e3757c323969645
Pavel Sidorovich <pavsid2001@mail.ru>
2022-01-21 21:14:11 +0300

    add validator config and resource bundles

--- service/src/main/java/com/epam/esm/gcs/dto/TagDto.java
+++ service/src/main/java/com/epam/esm/gcs/dto/TagDto.java
@@ -5,13 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class TagDto {
 
+    @Null(message = "{model.field.not.null}")
     private Long id;
+
+    @NotBlank(message = "{model.field.null}")
+    @Size(max = 128, message = "{model.field.size.max}")
     private String name;
 
 }
-
