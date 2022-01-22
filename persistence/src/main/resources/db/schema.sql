661ac440f0d3e05b687816c38c8b0ebb6c2cbbbd
Pavel Sidorovich <pavsid2001@mail.ru>
2022-01-22 15:25:38 +0300

    edit test-data and database scripts

--- persistence/src/main/resources/db/schema.sql
+++ persistence/src/main/resources/db/schema.sql
@@ -2,7 +2,7 @@ create table gift_certificate
 (
     id               bigserial
         constraint gift_certificate_pk
-            primary key,
+        primary key,
     name             varchar(128)  not null,
     description      varchar(256)  not null,
     price            numeric(8, 2) not null,
@@ -18,7 +18,7 @@ create table tag
 (
     id   bigserial
         constraint tag_pk
-            primary key,
+        primary key,
     name varchar(128) not null
 );
 
@@ -27,3 +27,21 @@ create unique index tag_id_uindex
 
 create unique index tag_name_uindex
     on tag (name);
+
+create table gift_certificates_by_tags
+(
+    id                  bigserial
+        constraint gift_certificates_by_tags_pk
+        primary key,
+    gift_certificate_id bigint not null
+        constraint gift_certificates_by_tags_gift_certificate_id_fk
+        references gift_certificate
+        on update cascade on delete cascade,
+    tag_id              bigint not null
+        constraint gift_certificates_by_tags_tag_id_fk
+        references tag
+        on update cascade on delete cascade
+);
+
+create unique index gift_certificates_by_tags_id_uindex
+    on gift_certificates_by_tags (id);
--- persistence/src/main/resources/db/test-data.sql
+++ persistence/src/main/resources/db/test-data.sql
@@ -1,6 +1,20 @@
-INSERT INTO public.tag (name)
+INSERT INTO tag (name)
 VALUES ('Gift');
-INSERT INTO public.tag (name)
+INSERT INTO tag (name)
 VALUES ('Car');
-INSERT INTO public.tag (name)
-VALUES ('Third tag');
+INSERT INTO tag (name)
+VALUES ('Family');
+INSERT INTO tag (name)
+VALUES ('Sports');
+INSERT INTO tag (name)
+VALUES ('Health');
+INSERT INTO tag (name)
+VALUES ('New Year');
+INSERT INTO tag (name)
+VALUES ('Balloon');
+INSERT INTO tag (name)
+VALUES ('Animal');
+INSERT INTO tag (name)
+VALUES ('Food');
+INSERT INTO tag (name)
+VALUES ('Clothes');
