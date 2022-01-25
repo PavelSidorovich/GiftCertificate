a15ffa7426647d86d33496c6296908bb774d6b1a
Pavel Sidorovich <pavsid2001@mail.ru>
2022-01-25 16:42:29 +0300

    add validators to certificate dto

--- service/src/main/java/com/epam/esm/gcs/dto/GiftCertificateDto.java
+++ service/src/main/java/com/epam/esm/gcs/dto/GiftCertificateDto.java
@@ -1,21 +1,45 @@
 package com.epam.esm.gcs.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
 public class GiftCertificateDto {
 
+    @Null(message = "{model.field.not.null}")
     private Long id;
+
+    @NotBlank(message = "{model.field.null}")
+    @Size(max = 128, message = "{model.field.size.max}")
     private String name;
+
+    @Size(max = 256, message = "{model.field.size.max}")
+    @NotNull(message = "{model.field.null}")
     private String description;
+
+    @Positive(message = "{model.field.positive}")
+    @NotNull(message = "{model.field.null}")
     private BigDecimal price;
+
+    @Positive(message = "{model.field.positive}")
+    @NotNull(message = "{model.field.null}")
     private Integer duration;
+
     private LocalDateTime createDate;
     private LocalDateTime lastUpdateDate;
     private List<TagDto> tags;
