78d9856a978f93e310219d1db6a1942fe22a0a74
Pavel Sidorovich <pavsid2001@mail.ru>
2022-02-15 13:43:40 +0300

    decompose package persistence to another two: persistence and repository

--- persistence/src/main/java/com/epam/esm/gcs/util/impl/QueryLimiter.java
+++ persistence/src/main/java/com/epam/esm/gcs/util/impl/QueryLimiter.java
@@ -9,12 +9,13 @@ public class QueryLimiter implements Limiter {
     private static final int DEFAULT_LIMIT = 20;
     private static final int DEFAULT_OFFSET = 0;
     private static final int MIN_LIMIT = 1;
+    private static final int MAX_LIMIT = 100;
 
     private Integer limit;
     private Integer offset;
 
     public QueryLimiter(Integer limit, Integer offset) {
-        this.limit = limit == null || limit < MIN_LIMIT
+        this.limit = limit == null || limit < MIN_LIMIT || limit > MAX_LIMIT
                 ? DEFAULT_LIMIT : limit;
         this.offset = offset == null || offset < DEFAULT_OFFSET?
                 DEFAULT_OFFSET : offset;
