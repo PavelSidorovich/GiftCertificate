367278cea2d5b6f0a141f892169f98cfbdb35c8a
Pavel Sidorovich <pavsid2001@mail.ru>
2022-02-17 01:56:39 +0300

    fix: add cost audit jpa

--- persistence/src/main/java/com/epam/esm/gcs/model/PurchaseModel.java
+++ persistence/src/main/java/com/epam/esm/gcs/model/PurchaseModel.java
@@ -55,6 +55,7 @@ public class PurchaseModel {
     @PrePersist
     public void onPrePersist() {
         purchaseDate = LocalDateTime.now();
+        cost = certificate.getPrice();
     }
 
     @Override
