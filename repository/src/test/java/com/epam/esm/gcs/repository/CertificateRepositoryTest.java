409c755df630720b52c3986c840f36d719210940
Pavel Sidorovich <pavsid2001@mail.ru>
2022-03-28 20:23:55 +0300

    fix: edit tests

--- repository/src/test/java/com/epam/esm/gcs/repository/CertificateRepositoryTest.java
+++ repository/src/test/java/com/epam/esm/gcs/repository/CertificateRepositoryTest.java
@@ -148,20 +148,20 @@ class CertificateRepositoryTest {
         certificateRepository.save(getTestGiftCertificates(tags3).get(2));
 
         final List<CertificateModel> actual1 =
-                certificateRepository.findByTags(Collections.emptyList(), 0L, pageRequest);
+                certificateRepository.findByTags(Collections.emptyList(), pageRequest);
         final List<CertificateModel> actual2 =
-                certificateRepository.findByTags(List.of("tagname1"), 1L, pageRequest);
+                certificateRepository.findByTags(List.of("tagname1"), pageRequest);
         final List<CertificateModel> actual3 =
-                certificateRepository.findByTags(List.of("tagname3", "tagname4"), 2L, pageRequest);
+                certificateRepository.findByTags(List.of("tagname3", "tagname4"), pageRequest);
         final List<CertificateModel> actual4 =
-                certificateRepository.findByTags(List.of("tagname4"), 1L, pageRequest);
+                certificateRepository.findByTags(List.of("tagname4"), pageRequest);
 
         assertNotNull(actual1);
         assertEquals(0, actual1.size());
         assertNotNull(actual2);
         assertEquals(2, actual2.size());
         assertNotNull(actual3);
-        assertEquals(1, actual3.size());
+        assertEquals(2, actual3.size());
         assertNotNull(actual4);
         assertEquals(1, actual4.size());
     }
